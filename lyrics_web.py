773c00787edd1b6fd0329eb0a79f312b8d2a5513
Jassimkabir <jassim@hamon.in>
2022-04-07 18:09:43 +0530

    fetched song name and lyrics using jquery

--- lyrics_web.py
+++ lyrics_web.py
@@ -10,14 +10,16 @@ def hello():
 @app.route("/artists")
 def get_artists():
     artists = get_data.get_all_artist()
-    return jsonify(artists)
+    artists_arr = [{'id':i[0], "name":i[1]} for i in artists]
+    return jsonify(artists_arr)
 
 @app.route("/songs/<int:aid>")
 def list_all_songs(aid):
     songs= get_data.get_all_songs(aid)
-    singer= get_data.singer(aid)
-    artists = get_data.get_all_artist()
-    return render_template("songlist.html",artists=artists,songs=songs,singer=singer,active_artrist=aid)
+    songs_arr = [{'id':i[1], "name":i[0]} for i in songs]
+    # singer= get_data.singer(aid)
+    # artists = get_data.get_all_artist()
+    return jsonify(songs_arr)
 
 @app.route("/songs/<int:aid>/lyrics/<int:sid>")
 def lyrics(sid,aid):
@@ -25,7 +27,8 @@ def lyrics(sid,aid):
     songs= get_data.get_all_songs(aid)
     singer= get_data.singer(aid)
     artists = get_data.get_all_artist()
-    return render_template("lyrics.html",lyrics=lyrics,artists=artists,songs=songs,singer=singer,active_artrist=aid,active_song=sid)
+    print(lyrics)
+    return jsonify(lyrics)
 
 if __name__=="__main__":
     app.run(debug=True) 
--- static/site.js
+++ static/site.js
@@ -1,8 +1,41 @@
 function main() {
   $.get({
     url: 'http://127.0.0.1:5000/artists',
-    success: (data) => $('div.artist_list').html(data),
+    success: (data) => {
+      list = '';
+      data.forEach((element) => {
+        list +=
+          `<li class="artistbox" value=${element.id}>` + element.name + `</li>`;
+      });
+      tag = `<ul type="none">${list}</ul>`;
+      $('div.artist_list').html(tag);
+      // console.log(data);
+    },
+  });
+  $(document).on('click', 'li.artistbox', function () {
+    $.get({
+      url: `http://127.0.0.1:5000/songs/${this.value}`,
+      success: (data) => {
+        list = '';
+        data.forEach((element) => {
+          list +=
+            `<li class="songbox" id=${element.id}>` + element.name + `</li>`;
+        });
+        tag = `<ul type="none">${list}</ul>`;
+        $('div.song_list').html(tag);
+        // console.log(data);
+      },
+    });
+  });
+  $(document).on('click', 'li.songbox', function () {
+    $.get({
+      url: `http://127.0.0.1:5000/songs/${this.value}/lyrics/${this.id}`,
+      success: (data) => {
+        lyrics = `<pre><p>${data}</p></pre>`;
+        $('div.lyrics').html(lyrics);
+        // console.log(data);
+      },
+    });
   });
 }
-
 $(main);
